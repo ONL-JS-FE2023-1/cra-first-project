14fe9928f30eea56f3cd31c084ba2fe909dce6ea
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-17 20:48:34 +0300

    Todo list task resolve

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todoList: [
+                {
+                    id: 1,
+                    text: "go to party"
+                },
+                {
+                    id: 2,
+                    text: "meet friends"
+                },
+                {
+                    id: 3,
+                    text: "have a walk"
+                }
+            ]
+        }
+    }
+
+    renderLi() {
+        const { todoList } = this.state;
+        return todoList.map((task) => <li key={task.id}>{task.text}</li>)
+    }
+
+    render() {
+        return (
+            <>
+                <h1>TODO LIST</h1>
+
+                <ul>
+                    {this.renderLi()}
+                </ul>
+            </>
+        )
+    }
+}
+
+export default TodoList;
+
+/*
+
+
+В кожній li зробити кнопку, за натисненням на яку цей елемент списку має зникунти
+(тобто оновити стейт таким чином, щоб зник об'єкт в масиві, на кнопку видалення якого ми натиснули)
+
+*/
