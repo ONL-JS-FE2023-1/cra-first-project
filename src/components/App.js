67b0cd8bb3a45fd4b0d744484ea6319a05f5ff5a
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-26 19:11:59 +0300

    Add react-toastify + clipboard writeText

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
     return (
         <>
-            {/* window.close() - тестувати цей метод у новій вкладці */}
-            <button onClick={() => {window.close()}}>Close</button>
-            <button onClick={() => {window.open('https://github.com/')}}>Open</button>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+            />
+
+            {/* assign - просто завантажує нову сторінку */}
+            <button onClick={() => { window.location.assign('https://developer.mozilla.org') }}>Assign</button>
+            <button onClick={() => { window.location.reload() }}>Reload</button>
+            {/* replace - завантажує нову сторінку, при тому до попередньої ми вже повернутися не можемо */}
+            <button onClick={() => { window.location.replace('https://github.com/') }}>Replace</button>
+
+            <p>Lorem ipsum dolor sit amet</p>
+            <button
+                onClick={() => {
+                    window.navigator.clipboard.writeText('Lorem ipsum dolor sit amet');
+                    toast.success('Copied to clipboard');
+                }}
+            >Click to copy text</button>
         </>
     )
 }
