0eb41cdd7305c2caa33e26eb68df94dce1e4a092
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-31 20:02:28 +0300

    Formik validation + ErrorMessage

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
 import {SCHEMA} from '../../schemas/index';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const initialState = {
     email: '',
@@ -17,14 +16,18 @@ function SignUpForm(props) {
 
     
         return (
-            <Formik initialValues={initialState} onSubmit={handleSubmitToFormik}>
+            <Formik initialValues={initialState} onSubmit={handleSubmitToFormik} validationSchema={SCHEMA}>
                 {(formikProps) => {
                     return (
-                        <Form>
+                        <Form style={{display: 'flex', flexDirection: 'column'}}>
                             <Field placeholder='firstName' name='firstName'/>
+                            <ErrorMessage name='firstName' component="p" />
                             <Field placeholder='lastName' name='lastName'/>
+                            <ErrorMessage name='lastName' component="p" />
                             <Field placeholder='email' name='email'/>
+                            <ErrorMessage name='email' component="p" />
                             <Field type='password' placeholder='password' name='password'/>
+                            <ErrorMessage name='password' component="p" />
                             <button type='submit'>Send</button>
                         </Form>
                     );
@@ -34,3 +37,11 @@ function SignUpForm(props) {
 }
 
 export default SignUpForm;
+
+
+/*
+
+ДЗ.
+Зробити маленьку форму авторизації (email,pass) за допомогою Formik
+
+*/
