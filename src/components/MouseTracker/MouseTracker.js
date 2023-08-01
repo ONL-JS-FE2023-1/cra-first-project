15d1358d53b30d193da02b8831478fde19035c06
Roman Ratushnyi <freshcode1001@ukr.net>
2023-08-01 19:26:53 +0300

    Bast practice

--- src/components/MouseTracker/MouseTracker.js
+++ src/components/MouseTracker/MouseTracker.js
@@ -3,17 +3,13 @@ import React, { useState, useEffect } from 'react';
 const MouseTracker = (props) => {
     const [coordinates, setCoordinates] = useState({
         x: 0,
-        y: 0,
-        counter: 0
+        y: 0
     })
 
+    const [counter, setCounter] = useState(0);
+
     const clickHandler = () => {
-        setCoordinates((previousState) => {
-            return {
-                ...previousState,
-                counter: coordinates.counter + 1
-            }
-        })
+        setCounter(counter + 1);
     }
 
     useEffect(() => {
@@ -33,20 +29,17 @@ const MouseTracker = (props) => {
     }, []) */
 
     const tracker = (event) => {
-        setCoordinates((previousState) => {
-            return {
-                ...previousState,
-                x: event.clientX,
-                y: event.clientY
-            }
+        setCoordinates({
+            x: event.clientX,
+            y: event.clientY
         })
     }
 
     return (
-        <div onClick={clickHandler} style={{backgroundColor: 'black', color: 'white'}}>
+        <div onClick={clickHandler} style={{ backgroundColor: 'black', color: 'white' }}>
             <p>X: {coordinates.x}</p>
             <p>Y: {coordinates.y}</p>
-            <p>Counter: {coordinates.counter}</p>
+            <p>Counter: {counter}</p>
         </div>
     );
 }
