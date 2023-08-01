e690adb6987addd43ebdf97f7118f0cbcda75d8c
Roman Ratushnyi <freshcode1001@ukr.net>
2023-08-01 20:47:13 +0300

    useContext

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
 import React from "react";
-import NewUserList from "./UserList/NewUserList";
-import UserList from "./UserList/UserList";
+import Tree from './Tree/Tree.js';
+import { ThemeContext } from "../contexts/themeContext";
+import CONSTANTS from '../constants';
+import { UserContext } from '../contexts/userContext'
+const { THEMES } = CONSTANTS;
 
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            theme: THEMES.LIGHT,
+            user: {
+                firstName: 'John',
+                lastName: 'Doe'
+            }
+        }
+    }
+
+
     render() {
+        const { user, theme } = this.state;
         return (
-            <NewUserList />
+            <UserContext.Provider value={{user}}>
+                <ThemeContext.Provider value={{theme}}>
+                    <Tree />
+                </ThemeContext.Provider>
+            </UserContext.Provider>
         )
     }
 }
