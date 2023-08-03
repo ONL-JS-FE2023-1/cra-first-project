10777ca904e00c5e8295d2973e44f120e310afc8
Roman Ratushnyi <freshcode1001@ukr.net>
2023-08-03 18:07:53 +0300

    useMouseTracker

---
+++ src/hooks/MouseTracker/MouseTracker.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+const useMouseTracker = (props) => {
+    const [coordinates, setCoordinates] = useState({
+        x: 0,
+        y: 0
+    })
+
+    useEffect(() => {
+        window.addEventListener('mousemove', tracker);
+        return () => {
+            window.removeEventListener('mousemove', tracker);
+        }
+    }, [])
+
+    const tracker = (event) => {
+        setCoordinates({
+            x: event.clientX,
+            y: event.clientY
+        })
+    }
+
+    return ({coordinates});
+}
+
+export default useMouseTracker;
